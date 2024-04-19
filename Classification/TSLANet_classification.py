77078a3f40d2949049f94670410e1bd99f62fd7a
Emadeldeen Eldele <37911596+emadeldeen24@users.noreply.github.com>
2024-04-19 09:29:53 +0800

    Update TSLANet_classification.py

--- Classification/TSLANet_classification.py
+++ Classification/TSLANet_classification.py
@@ -16,7 +16,7 @@ from timm.models.layers import trunc_normal_
 from torchmetrics.classification import MulticlassF1Score
 
 from dataloader import get_datasets
-from utils import get_clf_report, save_copy_of_files, str2bool
+from utils import get_clf_report, save_copy_of_files, str2bool, random_masking_3D
 
 
 class ICB(L.LightningModule):
@@ -57,40 +57,6 @@ class PatchEmbed(L.LightningModule):
     def forward(self, x):
         x_out = self.proj(x).flatten(2).transpose(1, 2)
         return x_out
-
-
-# This code snippet is from PatchTST ...
-def random_masking_3D(xb, mask_ratio):
-    # xb: [bs x num_patch x dim]
-    bs, L, D = xb.shape
-    x = xb.clone()
-
-    len_keep = int(L * (1 - mask_ratio))
-
-    noise = torch.rand(bs, L, device=xb.device)  # noise in [0, 1], bs x L
-
-    # sort noise for each sample
-    ids_shuffle = torch.argsort(noise, dim=1)  # ascend: small is keep, large is remove
-    ids_restore = torch.argsort(ids_shuffle, dim=1)  # ids_restore: [bs x L]
-
-    # keep the first subset
-    ids_keep = ids_shuffle[:, :len_keep]  # ids_keep: [bs x len_keep]
-    x_kept = torch.gather(x, dim=1, index=ids_keep.unsqueeze(-1).repeat(1, 1, D))  # x_kept: [bs x len_keep x dim]
-
-    # removed x
-    x_removed = torch.zeros(bs, L - len_keep, D, device=xb.device)  # x_removed: [bs x (L-len_keep) x dim]
-    x_ = torch.cat([x_kept, x_removed], dim=1)  # x_: [bs x L x dim]
-
-    # combine the kept part and the removed one
-    x_masked = torch.gather(x_, dim=1,
-                            index=ids_restore.unsqueeze(-1).repeat(1, 1, D))  # x_masked: [bs x num_patch x dim]
-
-    # generate the binary mask: 0 is keep, 1 is remove
-    mask = torch.ones([bs, L], device=x.device)  # mask: [bs x num_patch]
-    mask[:, :len_keep] = 0
-    # unshuffle to get the binary mask
-    mask = torch.gather(mask, dim=1, index=ids_restore)  # [bs x num_patch]
-    return x_masked, x_kept, mask, ids_restore
 
 
 class Adaptive_Spectral_Block(nn.Module):
